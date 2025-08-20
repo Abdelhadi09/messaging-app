49b2dd3e72746dc4640e0a75850b15bbfa371c93
Abdelhadi09 <abaditrki@gmail.com>
2025-08-20 13:00:06 +0100

    fixed cors bug

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ const authRoutes = require('./routes/auth');
 const messageRoutes = require('./routes/messages');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+origin: 'https://messaging-app-iota-three.vercel.app'
+}));
+app.options('*', cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
